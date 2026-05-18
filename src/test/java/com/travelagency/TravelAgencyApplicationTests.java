68a90d397dde2767db7952660a9e66e1f128fd21
Adrian-1986 <adryhonor9@gmail.com>
2026-05-18 10:06:10 +0200

    test: add travel agency tests

---
+++ src/test/java/com/travelagency/TravelAgencyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.travelagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
